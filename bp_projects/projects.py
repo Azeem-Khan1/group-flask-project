37e047fe4f0a7f4ee44099953e66974d8f459a49
nVarap <nvarap@gmail.com>
2022-09-19 20:13:59 -0700

    changes made to the project names and paths

--- bp_projects/projects.py
+++ bp_projects/projects.py
@@ -5,19 +5,19 @@ app_projects = Blueprint('projects', __name__,
                 template_folder='templates/bp_projects/')
 
 # connects /kangaroos path to render kangaroos.html
-@app_projects.route('/portfolio/')
+@app_projects.route('/chinmay/')
 def portfolio():
-    return render_template("portfolio.html")
+    return render_template("chinmay.html")
 
 # connects /kangaroos path to render kangaroos.html
-@app_projects.route('/kangaroos/')
+@app_projects.route('/varalu/')
 def kangaroos():
-    return render_template("kangaroos.html")
+    return render_template("varalu.html")
 
-@app_projects.route('/walruses/')
+@app_projects.route('/collin/')
 def walruses():
-    return render_template("walruses.html")
+    return render_template("collin.html")
 
-@app_projects.route('/hawkers/')
+@app_projects.route('/azeem/')
 def hawkers():
-    return render_template("hawkers.html")
+    return render_template("azeem.html")
