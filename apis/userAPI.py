94047132ab39f22819918bc47817b28a758614e2
nVarap <nvarap@gmail.com>
2022-11-18 14:01:33 -0800

    wifeiofjwoijief

---
+++ apis/userAPI.py
@@ -0,0 +1,98 @@
+from flask import Blueprint, jsonify  # jsonify creates an endpoint response object
+from flask_restful import Api, Resource # used for REST API building
+import requests  # used for testing 
+import random
+import wikipediaapi
+app_api = Blueprint('api', __name__,
+                   url_prefix='/api/')
+
+# API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
+api = Api(app_api)
+
+'''
+I combined both in order to support both endpoints on the same area. may allow us to pivot should it be necessary
+basically tricks the server into running both ends
+'''
+class CardAPI:
+    local_dic = {}
+    def addUsr(name, username, password, diction):
+        diction["name"] = name
+        diction["data"] = {
+            "user" : username,
+            "password" : password,
+            "bio" : "",
+            "bday" : "",
+            "interests" : ""
+        }
+        
+
+    class _Create(Resource):
+        def post(self, name, username, password): # simply creates the endpoint, dne otherwise
+            CardAPI.addUsr(name, username, password, CardAPI.local_dic)
+            pass
+            
+    # getJokes()
+    class _Read(Resource):
+        def get(self):
+            return jsonify(CardAPI.local_dic) # init wikipedia by default
+
+    class _WikiRead(Resource):
+        def get(self):
+            return jsonify(CardAPI.serialize('wikipedia')) # init wikipedia by default
+
+    # getJoke(id)
+    class _ReadWithName(Resource): # read when url have name query satisfied
+        def get(self, name):
+            return jsonify(CardAPI.serialize(name))# otherwise check with name
+    class _Delete(Resource):
+        def get(self, id):
+            key = CardAPI.local_dic.pop(id, None)
+            return jsonify({"list" : key, "dict":CardAPI.local_dic})
+
+    
+    # getJoke(id)
+   # class _ReadWithName(Resource): # read when url have name query satisfied
+    #    def get(self, name):
+     #       return jsonify()# otherwise check with name
+
+    # getRandomJoke()
+    #class _ReadRandom(Resource):
+     #   def get(self):
+      #      return jsonify() # this exists for some reason
+    
+
+    # building RESTapi resources/interfaces, these routes are added to Web Server
+    api.add_resource(_Create, '/card/create/<string:front>_<string:back>')
+    api.add_resource(_Read, '/card/')
+    api.add_resource(_WikiRead, '/wiki/')
+    api.add_resource(_ReadWithName, '/wiki/<string:name>')
+    api.add_resource(_Delete, 'card/delete/<int:id>')
+    
+if __name__ == "__main__": # THIS ONLY RUNS IF YOU RUN THE FILE, NOT IF YOU OPEN IN A TAB. ONLY USE FOR DEBUGGING
+    # server = "http://127.0.0.1:5000" # run local
+    server = 'https://flask.nighthawkcodingsociety.com' # run from web
+    url = server + "/api/auth"
+    responses = []  # responses list
+
+    # get count of jokes on server
+    count_response = requests.get(url+"/count")
+    count_json = count_response.json()
+    count = count_json['count']
+
+    # update likes/dislikes test sequence
+    num = str(random.randint(0, count-1)) # test a random record
+    responses.append(
+        requests.get(url+"/"+num)  # read joke by id
+        ) 
+    # obtain a random joke
+    responses.append(
+        requests.get(url+"/random")  # read a random joke
+        ) 
+
+    # cycle through responses
+    for response in responses:
+        print(response)
+        try:
+            print(response.json())
+        except:
+            print("unknown error")
